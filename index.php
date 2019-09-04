9d66c63a1161ac8243aaf8545c4de8197dc14e3e
Jens <jens@jensz12.com>
2019-09-04 16:26:53 +0200

    jquery 3.4.1

--- index.php
+++ index.php
@@ -338,7 +338,7 @@ a:hover, a:active {
     </div>
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"crossorigin="anonymous"></script>
 </body>
