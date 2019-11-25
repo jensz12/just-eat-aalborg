626276cb3827cb97df9c1c6b2159e21afca62a43
Jens Møller <jens@jensz12.com>
2019-11-25 02:00:06 +0100

    Add files via upload

--- views/info.php
+++ views/info.php
@@ -4,6 +4,19 @@
     <li>Bilerne køre på 95 benzin</<li>
     <li>Knappen til at åbne tankdækslet, sidder ved døren til føresæddet, nede ved gulvet</li>
     <li>Dæktrykket på fordækkene skal være 2.3 bar / 33 PSI. Dæktrykket på bagdækkene skal være 2.2 bar / 32 PSI.</li>
+    </ul>
+    <h3>Nummerplader</h3>
+    <ul>
+    <li>Aab01: BX 29 407</li>
+    <li>Aab02: BS 95 015</li>
+    <li>Aab03: BT 88 896</li>
+    <li>Aab04: BT 88 841</li>
+    <li>Aab05: CE Resten kommer snart</li>
+    </ul>
+</div>
+<div class="jumbotron">
+  <h2>Just Eat Dresscode</h2>
+    <img src="https://justeat.jensz12.com/img/andet/1440x2000-OC-Plakat-JE.png" class="img-fluid" alt="plakat">
 </div>
 <div class="jumbotron">
   <h2>Koder til telefoner, Slack osv...</h2>
@@ -11,6 +24,7 @@
     <li>Koderne til telefoner og Google konto kan findes i WikiEatia, som du kan hente via <a href="https://justeat.planday.dk">Planday</a>. Du kan også altid spørge på Slack efter dem</li>
     <li>Koden til bommene bag BurgerKing i Aalborg C er 531E</li>
     <li>Koden til tankkort er den samme som til telefon</li>
+    </ul>
 </div>
 <div class="jumbotron">
   <h2>Problemer med Google Maps</h2>
@@ -18,10 +32,12 @@
     <li>Virker Google Maps ikke, så er der hermed en guide til hvordan du forhåbentlig kan løse dem</a></li>
     <li>Start med at tjekke om du er logget ind på Google kontoen. Det er 99% tiden det der løser problemet. Du kan gøre dette ved at åbne Google Play, og se om du bliver bedt om at logge ind. Hvis du gør, så log ind, og se om dette løser problemet.</li>
     <li>Tjek også meget gerne om Google Maps er opdateret.</li>
+    </ul>
 </div>
 <div class="jumbotron">
   <h2>Telefonnummre</h2>
     <ul>
     <li>OC: <a href="tel:89888215">89888215</a></li>
     <li>Kundeservice: <a href="tel:70208590">70208590</a></li>
+    </ul>
 </div>
